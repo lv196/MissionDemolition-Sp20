f041fa0579d5d8c467dd802dc3e63ae26e0072b8
lv196 <lv196@scarletmail.rutgers.edu>
2020-02-18 01:30:08 -0500

    Slingshot Active

---
+++ Assets/Slingshot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+    public GameObject launchPoint;
+
+    private void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+
+    void OnMouseEnter() {
+        // print( "Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
+        }
+
+        void OnMouseExit() {
+        // print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
+        }
+
+}
