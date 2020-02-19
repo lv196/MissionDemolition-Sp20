f91ca52cdbedd76de34363559fd815777738b9bb
lv196 <lv196@scarletmail.rutgers.edu>
2020-02-19 01:01:11 -0500

    Making a Follow Camera

---
+++ Assets/FollowCam.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    static public GameObject POI; //The static point of interest that the camera should follow (projectile)
+
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
+
+    [Header("Set Dynamically")]
+    public float camZ; //The desired z pos of the camera
+
+    private void Awake()
+    {
+        camZ = this.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //if there's only one line following an if, it doesn't need braces
+        if (POI == null) return; //return if there is no poi, the rest of code is not executed
+
+        //Get the position of the poi
+        Vector3 destination = POI.transform.position;
+        //Limit the X & Y to minimum values 
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        //Interpolate from the current camera Position toward destination
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        //Force destination.z to be camZ to keep the camera far enough away
+        destination.z = camZ;
+        //set the camera to the destination
+        transform.position = destination;
+        //Set the orthographicSize of the Camera to keep Ground in view
+        Camera.main.orthographicSize = destination.y + 10;
+    }
+}
