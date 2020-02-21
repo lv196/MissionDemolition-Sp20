21115e18f1107f154faa70e4bce8b7c755365236
lv196 <lv196@scarletmail.rutgers.edu>
2020-02-20 20:42:33 -0500

    Adding Projectile Trail

--- Assets/__Scripts/FollowCam.cs
+++ Assets/__Scripts/FollowCam.cs
@@ -23,10 +23,36 @@ public class FollowCam : MonoBehaviour
     void FixedUpdate()
     {
         //if there's only one line following an if, it doesn't need braces
-        if (POI == null) return; //return if there is no poi, the rest of code is not executed
+        //-- if (POI == null) return; //return if there is no poi, the rest of code is not executed
 
         //Get the position of the poi
-        Vector3 destination = POI.transform.position;
+        //-- Vector3 destination = POI.transform.position;
+
+        Vector3 destination;
+        //if there is no poi, return to P:[0,0,0]
+        if (POI == null)
+        {
+            destination = Vector3.zero;
+        }
+        else
+        {
+            //Get the position of the POI
+            destination = POI.transform.position;
+            //If poi is a Projectile, check to see if its at rest
+
+            if (POI.tag == "Projectile")
+            {
+                //if it is sleeping (that is, not moving)
+                if (POI.GetComponent<Rigidbody>().IsSleeping())
+                {
+                    //return to default view
+                    POI = null;
+                    //in the next update
+                    return;
+                }
+            }
+        }
+
         //Limit the X & Y to minimum values 
         destination.x = Mathf.Max(minXY.x, destination.x);
         destination.y = Mathf.Max(minXY.y, destination.y);
--- Assets/__Scripts/Slingshot.cs
+++ Assets/__Scripts/Slingshot.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class Slingshot : MonoBehaviour {
+    static private Slingshot S;
     //fields set in the Unity Inspector pane
     [Header("Set in Inspector")]
     public GameObject prefabProjectile;
@@ -16,8 +17,20 @@ public class Slingshot : MonoBehaviour {
 
     private Rigidbody projectileRigidbody;
 
-    private void Awake()
+
+    static public Vector3 LAUNCH_POS
     {
+        get
+        {
+            if (S == null) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
+
+
+    void Awake()
+    {
+        S = this;
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
